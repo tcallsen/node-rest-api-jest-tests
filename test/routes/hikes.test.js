4f1c1779fac8a5a2a882537d34f38c8054f1c628
Taylor Callsen <gritto2008@gmail.com>
2022-03-21 15:55:43 -0700

    adding first jest test with hoisting enabled

--- test/routes/hikes.test.js
+++ test/routes/hikes.test.js
@@ -1,13 +1,18 @@
-const request = require('supertest');
-const app = require('../../app');
+import request from 'supertest';
+
+import app from '../../app.js';
+import * as hikeService from '../../services/hikeService.js';
 
 describe('Hikes Route Tests', () => {
 
   test('Should return a list of hikes', async () => {
+    const getRecentHikesMock = jest.spyOn(hikeService, 'getRecentHikes').mockImplementation(() => Promise.resolve({hikes: []}));
+    
     const response = await request(app).get("/hikes");
     expect(response.statusCode).toBe(200);
+    expect(response.body.hikes.length).toBe(0);
+    // expect(getRecentHikesMock).toHaveBeenCalledTimes(1);
   });
-
   
   test('Should return 401 for non-supported routes', async () => {
     const rootRouteResponse = await request(app).get("/");
